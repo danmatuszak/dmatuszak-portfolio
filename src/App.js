5519efe55e9354df1c5e8539ea7bd0c2e84da3c3
danmatuszak <dmatuszak@ucdavis.edu>
2023-06-18 22:01:23 -0700

    add navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
+import { useState, useEffect } from'react';
+import useMediaQuery from './hooks/useMediaQuery';
+import Navbar from './scenes/Navbar';
+import DotGroup from './scenes/DotGroup';
+import Landing from './scenes/Landing';
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState('home');
+  const [isTopOfPage, setIsTopOfPage] = useState(false);
+  const isAboveMediumScreens = useMediaQuery('(min-width: 1060px)');
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) setIsTopOfPage(true);
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [])
+
   return (
-    <div className="App">
+    <div className="app bg-deep-blue">
+      <Navbar
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      {/* space on sides, centered.  md breakpoint*/}
+      <div className="w-5/6 mx-auto md:h-full">
+        {isAboveMediumScreens && (
+          <DotGroup
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage}
+          />
+        )}
+        <Landing setSelectedPage={setSelectedPage} />
+      </div>
     </div>
   );
 }
