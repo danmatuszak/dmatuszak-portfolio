b3bf6ffa4aeef070c03293f6637604c9deac6843
danmatuszak <dmatuszak@ucdavis.edu>
2023-06-20 23:02:05 -0700

    Added contact page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Landing from './scenes/Landing';
 import MySkills from './scenes/MySkills';
 import Projects from './scenes/Projects';
 import Testimonials from './scenes/Testimonials';
+import Contact from './scenes/Contact';
+import Footer from './scenes/Footer';
 import LineGradient from './components/LineGradient';
 
 function App() {
@@ -50,6 +52,11 @@ function App() {
       <div className="w-5/6 mx-auto md:h-full">
         <Testimonials />
       </div>
+      <LineGradient />
+      <div className="w-5/6 mx-auto md:h-full">
+        <Contact />
+      </div>
+      <Footer />
     </div>
   );
 }
